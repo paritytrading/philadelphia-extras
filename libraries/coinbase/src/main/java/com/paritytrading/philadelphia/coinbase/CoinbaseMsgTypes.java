a9a677636f2e2ad9dc2ca90e5edfc3c5f90c4516
Jussi Virtanen <jussi.k.virtanen@gmail.com>
2019-10-14 18:53:59 +0100

    philadelphia-coinbase: Add missing message types

---
+++ libraries/coinbase/src/main/java/com/paritytrading/philadelphia/coinbase/CoinbaseMsgTypes.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2019 Philadelphia authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.paritytrading.philadelphia.coinbase;
+
+/**
+ * Message types for Coinbase Pro FIX API and Coinbase Prime FIX API.
+ */
+public class CoinbaseMsgTypes {
+
+    public static final String OrderCancelBatchRequest = "U4";
+    public static final String OrderCancelBatchReject  = "U5";
+    public static final String NewOrderBatch           = "U6";
+    public static final String NewOrderBatchReject     = "U7";
+
+}
