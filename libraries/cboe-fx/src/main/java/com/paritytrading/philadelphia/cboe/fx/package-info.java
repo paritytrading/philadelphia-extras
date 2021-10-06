3277f98910c69c17a5204b5cf7c16782fceed47c
Jussi Virtanen <jussi.k.virtanen@gmail.com>
2021-10-06 17:10:19 +0300

    philadelphia-cboe-fx: Clean up package description

--- libraries/cboe-fx/src/main/java/com/paritytrading/philadelphia/cboe/fx/package-info.java
+++ libraries/cboe-fx/src/main/java/com/paritytrading/philadelphia/cboe/fx/package-info.java
@@ -15,6 +15,6 @@
  */
 
 /**
- * This package contains an implementation of Cboe FX FIX Proxy 5.1.4.
+ * An implementation of Cboe FX FIX Proxy 5.1.4.
  */
 package com.paritytrading.philadelphia.cboe.fx;
