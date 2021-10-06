dab6acb177776fa18a6fb3285ea4ca2435ee591c
Jussi Virtanen <jussi.k.virtanen@gmail.com>
2021-10-06 17:10:58 +0300

    philadelphia-coinbase: Clean up package description

--- libraries/coinbase/src/main/java/com/paritytrading/philadelphia/coinbase/package-info.java
+++ libraries/coinbase/src/main/java/com/paritytrading/philadelphia/coinbase/package-info.java
@@ -15,7 +15,6 @@
  */
 
 /**
- * This package contains an implementation of Coinbase Pro FIX API and
- * Coinbase Prime FIX API.
+ * An implementation of Coinbase Pro FIX API and Coinbase Prime FIX API.
  */
 package com.paritytrading.philadelphia.coinbase;
